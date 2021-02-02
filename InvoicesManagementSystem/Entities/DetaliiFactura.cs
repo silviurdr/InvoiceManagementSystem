5498950bcfaddd96273d7462d5c0e47d95c44904
Silviu <silviu.catrina@gmail.com>
2021-02-02 10:22:40 +0200

    Initial commit

---
+++ InvoicesManagementSystem/Entities/DetaliiFactura.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InvoicesManagementSystem.Entities
+{
+    public class DetaliiFactura
+    {
+        public int IdDetaliiFactura { get; set; }
+        public int IdLocatie { get; set; }
+        public int IdFactura { get; set; }
+        public string NumeProdus { get; set; }
+        public decimal Cantitate { get; set; }
+        public decimal PretUnitar { get; set; }
+        public decimal Valoare { get; set; }
+    }
+}
---
+++ InvoicesManagementSystem/Entities/Factura.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InvoicesManagementSystem.Entities
+{
+    public class Factura
+    {
+        public int IdFactura { get; set; }
+        public int IdLocatie { get; set; }
+        public string NumarFactura { get; set; }
+        public DateTime DataFactura { get; set; }
+        public string NumeClient { get; set; }
+    }
+}
